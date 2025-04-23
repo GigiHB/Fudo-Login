2247a4665c3d7d482f0f72fe71847080138c5534
GigiHB <giselle.brenes96@gmail.com>
2025-04-22 18:07:22 -0600

    Update fudoCreds.py

--- fudoCreds.py
+++ fudoCreds.py
@@ -4,7 +4,6 @@ url_users = "https://api.fu.do/users?a=-1"
 headers = {
 	"Accept": "application/json",
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
-    "token":"eyJhbGciOiJIUzI1NiJ9.eyJ1aSI6MSwidWEiOnRydWUsInVyIjoxLCJ1bCI6ImFkbWluQGNlc2FycmVzdGF1cmFudHMiLCJhaSI6Mjc3NDAxLCJzaWMiOjExLCJjaSI6IjQiLCJleHAiOjE3NDU0NTIwNjN9.Y6HdlosJ_PY6a1Vpp-kqotyZguv4QrjqUctOhVSDgCo"
 }
 
 payload = {
